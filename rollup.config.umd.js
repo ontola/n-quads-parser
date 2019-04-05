80af2136c20370fb0d5d6da6270355f2de08d88e
Fletcher91 <thom@argu.co>
2019-04-05 16:32:36 +0200

    [IMP] Use es5 for umd build

--- rollup.config.umd.js
+++ rollup.config.umd.js
@@ -4,11 +4,16 @@ import typescript from "rollup-plugin-typescript2";
 export default {
     input: "src/index.ts",
     output: [
-        { file: "dist/n-quads-parser.es6.js", format: "es" },
+        { file: "dist/n-quads-parser.umd.js", name: "NQuadsParser", format: "umd" },
     ],
     plugins: [
         // Compile TypeScript files
         typescript({
+            tsconfigOverride: {
+                compilerOptions: {
+                    target: "es5",
+                },
+            },
             typescript: require("typescript"),
         }),
         // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
